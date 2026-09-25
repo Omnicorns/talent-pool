b16b212d67df4137c2d29240b6b0cb65776f11cf
Ant <tantoaloysius15@gmail.com>
2026-09-25 09:39:21 +0700

    feat: route talent portal entrypoint

--- src/main/java/com/example/talentpool/controller/SpaController.java
+++ src/main/java/com/example/talentpool/controller/SpaController.java
@@ -1,16 +1,24 @@
 package com.example.talentpool.controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SpaController {
+
     @GetMapping({
             "/backoffice",
             "/backoffice/**"
     })
     public String forwardBackoffice() {
         return "forward:/index.html";
+    }
+
+    @GetMapping({
+            "/talent",
+            "/talent/"
+    })
+    public String forwardTalentPortal() {
+        return "forward:/talent/index.html";
     }
 }
